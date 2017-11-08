5225f0f006d7334f510a2a22cc363ddad6296a52
zbxue <zibin_xue@163.com>
2017-11-09 02:01:51 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  selectedHero: Hero;
+  users = [ '1', '2', '3' ];
+  heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+// Hero class
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice'},
+  { id: 12, name: 'Narco'},
+  { id: 13, name: 'Bombasto'},
+  { id: 14, name: 'Celeritas'},
+  { id: 15, name: 'Magneta'},
+  { id: 16, name: 'RubberMan'},
+  { id: 17, name: 'Dynama'},
+  { id: 18, name: 'Dr IQ'},
+  { id: 19, name: 'Magma'},
+  { id: 20, name: 'Tornado'}
+];
